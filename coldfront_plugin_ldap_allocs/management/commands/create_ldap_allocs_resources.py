e1ad6d8b375cda130442ee005271105ca1515340
Cecilia Lau <cecilialau6776@gmail.com>
2025-03-24 18:04:12 -0400

    create command to add  attribute type choice

---
+++ coldfront_plugin_ldap_allocs/management/commands/create_ldap_allocs_resources.py
@@ -0,0 +1,16 @@
+import logging
+
+from coldfront.core.allocation.models import AttributeType, AllocationAttributeType
+
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "Add the `ldap-group-name` attribute type choice"
+
+    def handle(self, *args, **options):
+        text_at = AttributeType.objects.get(name="Text")
+        AllocationAttributeType.objects.get_or_create(
+            attribute_type=at, name="ldap-group-name"
+        )
